a85eb83d04afef4223bc7c7aafb1af76c1c5db5b
gen0x39 <migesan0319@gmail.com>
2021-11-11 17:33:56 +0900

    Fix testscript and test.py

--- train.sh
+++ train.sh
@@ -14,3 +14,15 @@ python3 train.py \
     --grad_clip 5 --drop_path_prob 0.3 --init_channels 36 \
     --momentum 0.9 --weight_decay 3e-5 --arch PCDARTS$i
 done
+
+for i in 72
+do
+python3 train.py \
+    --auxiliary \
+    --cutout \
+    --seed $i \
+    --layers 20 \
+    --epochs 600 --batch_size 64 --learning_rate 0.025 \
+    --grad_clip 5 --drop_path_prob 0.3 --init_channels 36 \
+    --momentum 0.9 --weight_decay 3e-5 --arch DSRNAJB$i
+done
