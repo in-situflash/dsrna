cd268cd8383f4d04e2d9c239c7fbd3c74ccee436
gen0x39 <migesan0319@gmail.com>
2021-11-11 17:34:28 +0900

    Add build script (test.sh)

---
+++ test.sh
@@ -0,0 +1,23 @@
+for i in 31
+do
+python3 test.py \
+--auxiliary \
+--cutout \
+--seed $i \
+--arch PCDARTS$i \
+--model_path EXP/pcdarts$i.pt \
+--layers 20 --init_channels 36 \
+--batch_size 64
+done
+
+for i in 72
+do
+python3 test.py \
+--auxiliary \
+--cutout \
+--seed $i \
+--arch DSRNAJB$i \
+--model_path EXP/dsrnajb$i.pt \
+--layers 20 --init_channels 36 \
+--batch_size 64
+done
