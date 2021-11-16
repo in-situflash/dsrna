c9eeef1afdcda0074b1770ce45a73d66e2a4686b
gen0x39 <migesan0319@gmail.com>
2021-11-16 17:03:40 +0900

    Add test script test_clean test_adv

---
+++ test_adv.sh
@@ -0,0 +1,27 @@
+# Test PC-DARTS
+for i in 31
+do
+python3 test.py \
+--auxiliary \
+--cutout \
+--seed $i \
+--arch PCDARTS$i \
+--model_path EXP/pcdarts$i.pt \
+--layers 20 --init_channels 36 \
+--batch_size 64 \
+--test_mode ADV
+done
+
+# Test DSRNA-JB
+for i in 72
+do
+python3 test.py \
+--auxiliary \
+--cutout \
+--seed $i \
+--arch DSRNAJB$i \
+--model_path EXP/dsrnajb$i.pt \
+--layers 20 --init_channels 36 \
+--batch_size 64 \
+--test_mode ADV
+done
