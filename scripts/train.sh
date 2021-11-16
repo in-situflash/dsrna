dc2d30593578525e241098a3b652ccde26130349
gen0x39 <migesan0319@gmail.com>
2021-11-16 21:38:11 +0900

    Make scriptdirectory

---
+++ scripts/train.sh
@@ -0,0 +1,30 @@
+#
+# in oiginal paper
+# batch size = 128
+#
+
+# Train PC-DARTS
+for i in 31
+do
+python3 ../train.py \
+    --auxiliary \
+    --cutout \
+    --seed $i \
+    --layers 20 \
+    --epochs 600 --batch_size 64 --learning_rate 0.025 \
+    --grad_clip 5 --drop_path_prob 0.3 --init_channels 36 \
+    --momentum 0.9 --weight_decay 3e-5 --arch PCDARTS$i
+done
+
+# Train DSRNA-JB
+for i in 72
+do
+python3 ../train.py \
+    --auxiliary \
+    --cutout \
+    --seed $i \
+    --layers 20 \
+    --epochs 600 --batch_size 64 --learning_rate 0.025 \
+    --grad_clip 5 --drop_path_prob 0.3 --init_channels 36 \
+    --momentum 0.9 --weight_decay 3e-5 --arch DSRNAJB$i
+done
---
+++ scripts/train_search.sh
@@ -0,0 +1,25 @@
+# Architecture Search PC-DARTS
+for i in 31
+do
+python3 ../train_search.py \
+--seed $i \
+--train_mode PC-DARTS \
+--layers 8 --epochs 50 --init_channels 16 \
+--learning_rate 0.025 --batch_size 128 --momentum 0.9 --weight_decay 3e-4 \
+--arch_learning_rate 3e-4 --arch_weight_decay 1e-3 
+done
+#--unrolled \
+
+
+# Architecture Search DSRNA-JB
+#for i in 72
+for i in 31
+do
+python3 ../train_search.py \
+--seed $i \
+--train_mode DSRNA-JB \
+--layers 8 --epochs 50 --init_channels 16 \
+--learning_rate 0.025 --batch_size 128 --momentum 0.9 --weight_decay 3e-4 \
+--arch_learning_rate 3e-4 --arch_weight_decay 1e-3 
+done
+#--unrolled \
